ebc5e93a660cadd283ea946ae68d759e9b69d38a
sajed <sizmamani@yahoo.com>
2018-11-11 10:22:46 +0800

    add request service wrapper and mock.json

---
+++ src/services/request.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+
+class request {
+  
+  constructor() {
+    let service = axios.create({});
+    service.interceptors.request.use(async (config) => {
+      return config
+    })
+    this.service = service;
+  }
+  
+
+  get(url, callback) {
+    return this.service.request({
+      method: 'GET',
+      url,
+      responseType: 'json',
+    }).then((response) => callback(response.data));
+  }
+}
+
+export default new request;
