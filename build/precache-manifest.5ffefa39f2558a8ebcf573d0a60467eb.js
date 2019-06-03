0a8787b87a4f8589d162ffa5aaa0f0e6744f537b
sajed <sizmamani@yahoo.com>
2019-06-03 12:32:55 +0200

    build

---
+++ build/precache-manifest.5ffefa39f2558a8ebcf573d0a60467eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d30d796ca816319041f9",
+    "url": "/static/js/main.d30d796c.chunk.js"
+  },
+  {
+    "revision": "067cb0c01949585db34c",
+    "url": "/static/js/1.067cb0c0.chunk.js"
+  },
+  {
+    "revision": "d30d796ca816319041f9",
+    "url": "/static/css/main.5b39fe59.chunk.css"
+  },
+  {
+    "revision": "00f4b374215c468011a2cc5bdf8892dd",
+    "url": "/index.html"
+  }
+];
